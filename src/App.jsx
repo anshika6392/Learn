61207f552a6c01416a720b28b92486ca6035e08c
anshika6392 <prachi639267@gmail.com>
2025-04-03 21:47:07 +0530

    todays changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ const App = () => {
     <div>
       <h1 className='text-red-600'>i am gullu trivedi</h1>
       <h2>This is new change</h2>
+       <button className='bg-amber-300 border-2 black'>SUBMIT</button>
+      <h2>hello</h2>
     </div>
   )
 }
