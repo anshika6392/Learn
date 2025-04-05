c34ed047d4b8bf1cb639bf7c035eb8ef025efce2
anshika6392 <prachi639267@gmail.com>
2025-04-05 21:47:58 +0530

    new change

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
       <h2>This is new change</h2>
        <button className='bg-amber-300 border-2 black'>SUBMIT</button>
       <h2>hello</h2>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate pariatur velit nam sint. Dignissimos sed nihil laborum eius molestiae doloribus voluptatibus, odit sequi veniam fuga sint natus exercitationem ea dolore?</p>
     </div>
   )
 }
